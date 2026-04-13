84c8ea63d3c811c250148a8142d62f305a0daefd
Andrew Phan <andrewphan@Andrews-MacBook-Air.local>
2026-04-12 18:47:29 -0700

    Fix MVTLayer worker and color logic strings

--- app.py
+++ app.py
@@ -52,53 +52,42 @@ if search_apn:
     t = df_all[df_all['APN'] == search_apn].iloc[0]
     st.session_state.view_state = pdk.ViewState(latitude=t['lat'], longitude=t['lon'], zoom=15)
 
-layer_id = "test_layer_v100"
-
-r_js = "properties.C_ID == 1 ? 34 : (properties.C_ID == 2 ? 245 : 239)"
-g_js = "properties.C_ID == 1 ? 197 : (properties.C_ID == 2 ? 158 : 68)"
-b_js = "properties.C_ID == 1 ? 94 : (properties.C_ID == 2 ? 11 : 68)"
-
 v_parts = []
 if show_prime:
     v_parts.append("properties.C_ID == 1")
-    if not only_irrigated: v_parts.append("properties.C_ID == 2")
+    if not only_irrigated: 
+        v_parts.append("properties.C_ID == 2")
 if show_marginal and not only_irrigated: 
     v_parts.append("properties.C_ID == 3")
 
 logic_chain = " || ".join(v_parts) if v_parts else "false"
-alpha_js = f"({logic_chain}) ? 100 : 0"
 
-    
-# 1. Simplify the JS strings for the sub-layer
-r_js_clean = "d.properties.C_ID == 1 ? 34 : (d.properties.C_ID == 2 ? 245 : 239)"
-g_js_clean = "d.properties.C_ID == 1 ? 197 : (d.properties.C_ID == 2 ? 158 : 68)"
-b_js_clean = "d.properties.C_ID == 1 ? 94 : (d.properties.C_ID == 2 ? 11 : 68)"
-alpha_js_clean = f"({logic_chain.replace('properties.', 'd.properties.')}) ? 100 : 0"
+r_js = "properties.C_ID == 1 ? 34 : (properties.C_ID == 2 ? 245 : 239)"
+g_js = "properties.C_ID == 1 ? 197 : (properties.C_ID == 2 ? 158 : 68)"
+b_js = "properties.C_ID == 1 ? 94 : (properties.C_ID == 2 ? 11 : 68)"
+alpha_js = f"({logic_chain}) ? 150 : 0"
 
 layer = pdk.Layer(
-    "TileLayer",
+    "MVTLayer",
     data="https://raw.githubusercontent.com/andrewphankt/farmland-roi/main/static/tiles/{z}/{x}/{y}.pbf",
-    id="parcel-tile-layer-v3",
-    loaders=["https://unpkg.com/@loaders.gl/mvt@3.4.4/dist/mvt-loader.umd.js"],
-    
-    # We use a pure JS function here to avoid the "Expected Comma" error
-    render_sub_layers=f"""
-        new GeoJsonLayer(props, {{
-            id: props.id + '-geojson',
-            data: props.data,
-            getFillColor: d => [{r_js_clean}, {g_js_clean}, {b_js_clean}, {alpha_js_clean}],
-            getLineColor: [255, 255, 255, 50],
-            lineWidthMinPixels: 1,
-            pickable: true
-        }})
-    """,
+    id="ag_layer_v_final",
     pickable=True,
+    auto_highlight=True,
+    binary=False,
+    load_options={
+        "mvt": {
+            "layers": ["all_counties_diamonds"]
+        }
+    },
+    get_fill_color=f"[{r_js}, {g_js}, {b_js}, {alpha_js}]",
+    get_line_color=[255, 255, 255, 30],
+    line_width_min_pixels=1,
 )
 
 deck = pdk.Deck(
     layers=[layer],
     initial_view_state=st.session_state.view_state,
-    map_style=None,
+    map_style="mapbox://styles/mapbox/satellite-v9",
     api_keys={"mapbox": st.secrets["MAPBOX_API_KEY"]},
     tooltip={
         "html": """
@@ -120,7 +109,8 @@ event = st.pydeck_chart(deck, on_select="rerun", selection_mode="single-object")
 if event and "selection" in event and event["selection"]["objects"]:
     all_selected = event["selection"]["objects"]
     if all_selected:
-        first_layer_objects = next(iter(all_selected.values()))
+        first_layer_key = next(iter(all_selected))
+        first_layer_objects = all_selected[first_layer_key]
         if first_layer_objects:
             selected_obj = first_layer_objects[0]
             apn_clicked = str(selected_obj.get("APN"))
